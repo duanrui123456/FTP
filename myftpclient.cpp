33e23e9dcc06752276d5db3ef04eed9136c03de5
Alex <alexp6114@gmail.com>
2014-02-11 11:35:38 -0500

    get command completed

--- myftpclient.cpp
+++ myftpclient.cpp
@@ -1,17 +1,46 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <unistd.h>
 #include <iostream>
 
-#define PORTNUM 3019
 #define BUF_SIZE 512
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+  // check for com line args
+  if(argc != 3)
+  {
+    cout << "myftp requires two command line arguments: the machine name where the server resides and the port number to connect to." << endl;
+    exit(-1);
+  }// if
+  
+  // retrieve server location
+  in_addr_t server_ip = inet_addr(argv[1]);
+
+  // call the user dumb for bad input
+  // note: inet_addr() returns -1 on error, but in_addr_t is unsigned, so -1 signed = 4294967295 unsigned
+  if(server_ip == 4294967295)
+  {
+    cout << argv[1] << " is not a valid ip address." << endl;
+    exit(-1);
+  }// if
+
+  // retrieve port number to listen on
+  int port_num = atoi(argv[2]);
+
+  // tell the user to screw off if he enters a bad port number, an atoi() error (-1) is inherently handled here
+  if(port_num < 1 || port_num > 65535)
+  {
+    cout << argv[2] << " is not a valid port number." << endl;
+    exit(-1);
+  }// if
+
   int tcp_socket = socket(AF_INET,SOCK_STREAM,0);
   if(tcp_socket < 0)
   {
@@ -21,8 +50,8 @@ int main(int argc, char* argv[])
   struct sockaddr_in clientaddr;
   
   clientaddr.sin_family = AF_INET;
-  clientaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  clientaddr.sin_port = htons(PORTNUM);
+  clientaddr.sin_addr.s_addr = server_ip; //inet_addr("127.0.0.1");
+  clientaddr.sin_port = htons(port_num);
   
   connect(tcp_socket,(struct sockaddr *)&clientaddr,sizeof(struct sockaddr));
 
@@ -35,11 +64,12 @@ int main(int argc, char* argv[])
     string command = "";
     printf("myftp>%c",' ');
     getline(cin, command);
-    // inputing nothing crashes server
+    
+    // inputting nothing crashes server
     if(command == "")
+    {
       continue;
-
-    // TODO make sure command consists of no more than two tokens
+    }// if
 
     send(tcp_socket,command.c_str(),BUF_SIZE,0);
     if(command == "quit")
@@ -50,12 +80,17 @@ int main(int argc, char* argv[])
     char message[BUF_SIZE];
     recv(tcp_socket,message,BUF_SIZE,0);
 
+    // don't print CONFIRM string
+    if((strcmp(message,CONFIRM)) == 0)
+    {
+      continue;
+    }// if
+    
+    // receive file
     if((strcmp(message, FSEND)) == 0)
     {
       // receive file name
       recv(tcp_socket,message,BUF_SIZE,0);
-      int file_size = 0;
-      char read_file[BUF_SIZE];
       FILE *file = fopen(message, "w");
 
       // keep receiving file until it reaches end
@@ -65,8 +100,15 @@ int main(int argc, char* argv[])
         {
             break;
         }// if
-        fwrite(message,sizeof(char),BUF_SIZE,file);
-        memset(read_file,' ',BUF_SIZE);
+        else
+        {
+          int char_count = BUF_SIZE;
+          while(message[char_count-1] == '\0')
+          {
+            char_count--;
+          }// while
+          fwrite(message,sizeof(char),char_count,file);
+        }// else
       }// while
       fclose(file);
     }// if
--- myftpserver.cpp
+++ myftpserver.cpp
@@ -10,15 +10,14 @@
 #include <string>
 #include <libgen.h>
 
-#define PORTNUM 3019
 #define BUF_SIZE 512
 
 using namespace std;
 
 /*	these are the function we need to implement
-	get
+	get----------done
 	put
-	delete
+	delete-------done
 	ls-----------done
 	cd-----------done
 	mkdir--------done
@@ -28,12 +27,38 @@ using namespace std;
 	use threads with pthreads package
 */
 
+const char CONFIRM[] = "Done";
+const char INVALCMD[] = "Invalid Command";
+const char ERROR[] = "Error Occurred";
+const char FSEND[] = "File Send";
+
 string ls();
 string pwd();
-void get(const int *socket, const char *ERROR, const char *FSEND, char* arg);
+void get(const int *socket, char* arg);
 
 int main(int argc, char* argv[])
 {
+  // check for cmd line arg
+  if(argc != 2)
+  {
+    cout << "myftpserver requires one command line argument: the port number to listen on." << endl;
+    exit(-1);
+  }// if
+  
+  // retrieve port number to listen on
+  int port_num = atoi(argv[1]);
+
+  // tell the user to screw off if he enters a bad port number
+  if(port_num < 1 || port_num > 65535)
+  {
+    cout << argv[1] << " is not a valid port number." << endl;
+    exit(-1);
+  }// if
+
+  // get home directory
+  char home_dir[BUF_SIZE];
+  getcwd(home_dir, BUF_SIZE);
+
   int tcp_socket = socket(AF_INET,SOCK_STREAM,0);
   if(tcp_socket < 0)
   {
@@ -46,12 +71,7 @@ int main(int argc, char* argv[])
   
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(PORTNUM);
-  
-  const char CONFIRM[] = "Done";
-  const char INVALCMD[] = "Invalid Command";
-  const char ERROR[] = "Error Occurred";
-  const char FSEND[] = "File Send";
+  servaddr.sin_port = htons(port_num);
   
   bind(tcp_socket,(struct sockaddr* )&servaddr,sizeof(struct sockaddr));
   listen(tcp_socket,5);
@@ -60,7 +80,7 @@ int main(int argc, char* argv[])
   // accept connections
   while(connect_socket = accept(tcp_socket,(struct sockaddr*)&dest,&socksize))
   {
-    printf("Incoming Transmission from %s\n",inet_ntoa(dest.sin_addr));
+    // printf("Incoming Transmission from %s\n",inet_ntoa(dest.sin_addr));
 
     // continue until remote user enters quit
     for(;;)
@@ -75,7 +95,7 @@ int main(int argc, char* argv[])
       
       if(strcmp(cmd, "get") == 0)
       {
-        get(&connect_socket,ERROR,FSEND,arg);
+        get(&connect_socket,arg);
         send(connect_socket,CONFIRM,BUF_SIZE,0);
       } // if
       else if(strcmp(cmd, "put") == 0){}// else if
@@ -129,11 +149,12 @@ int main(int argc, char* argv[])
       {
       	// end this connection
       	close(connect_socket);
+	      chdir(home_dir);
       	break;
       }// else if
       else
       {
-	       send(connect_socket,INVALCMD,sizeof(INVALCMD),0);
+        send(connect_socket,INVALCMD,sizeof(INVALCMD),0);
       }// else 	
     }// for
   }// while
@@ -178,7 +199,7 @@ string pwd()
   return string(return_msg);
 }// pwd
 
-void get(const int *socket, const char *ERROR, const char *FSEND, char* arg)
+void get(const int *socket, char* arg)
 {
   int file_size = 0;
   char read_file[BUF_SIZE];
@@ -193,13 +214,12 @@ void get(const int *socket, const char *ERROR, const char *FSEND, char* arg)
     send(*socket,FSEND,BUF_SIZE,0);
     // send file name
     send(*socket,basename(arg),BUF_SIZE,0);
-    
-    while((file_size = fread(read_file, sizeof(char), BUF_SIZE, file)) > 0)
+    while(file_size = fread(read_file, sizeof(char), BUF_SIZE, file) > 0)
     {
       send(*socket,read_file,BUF_SIZE,0);
-      memset(read_file,' ',BUF_SIZE);
+      memset(read_file,'\0',BUF_SIZE);
     }// while
     fclose(file);
   }// else
-}
+}// get
     
